b8f387e29958b82cd51b7d7d68f85e35c0696cc2
suki-suki <sukrit96singh@gmail.com>
2016-05-11 15:32:27 -0400

    working

--- project.js
+++ project.js
@@ -1,3 +1,27 @@
-function setup(){
-	createCanvas(windowWidth,windowHeight);
+var gif;
+var img;
+
+
+function setup() {
+  createCanvas(800, 600);
+  img = loadImage("images/first.jpg");
+}
+
+function draw() {
+  image(img, 0, 0);
+}
+
+function mouseClicked() {
+	if (image.loaded()) {
+			function setup(){
+				createCanvas(800, 600);
+				gif = loadGif("gifs/title.gif");
+			}
+		  function draw() {
+				background(0);
+					if (gif.loaded()) {
+					image(gif, 0, 0);
+					}
+				}
+			}
 }
